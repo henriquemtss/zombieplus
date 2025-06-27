5033740547de30d24ca914e7b4db9e1f5fca6f2e
henriquemtss <henriquegomesdematos08@gmail.com>
2025-06-27 00:42:55 -0300

    reports de riscos na nuvem

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['dot'],
+    ['playwright-tesults-reporter', {'tesults-target': process.env.TOKEN}]
+    // ['junit', { outputFile: 'test-results.xml' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,6 +37,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     baseURL: process.env.BASE_URL 
+    //viuwport: { width: 1280, height: 720 },
   },
 
   /* Configure projects for major browsers */
--- tests/support/database.js
+++ tests/support/database.js
@@ -16,7 +16,7 @@ export async function executeSQL(sqlScript){
         const client = await pool.connect()
 
         const result = await client.query(sqlScript)
-        console.log(result.rows);
+       
     } catch (error) {
         console.log('Erro ao executar SQL script:', error)
     }
